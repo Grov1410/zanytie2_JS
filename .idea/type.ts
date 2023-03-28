789e83369b76fb03e4302228cc7e710bbccc96a4
Gurin Roman <rovgurin@gmail.com>
2023-03-29 00:34:07 +0300

    first commit

---
+++ .idea/type.ts
@@ -0,0 +1,15 @@
+//Вывести в терминал строку “Hello World!”
+console.log('Hello World!')
+
+//Создать переменную для каждого типа данных
+const name: string = 'Roman'
+const surname: undefined = undefined
+let age: number = 32
+let isAdmin: boolean = true
+let experience: object = null
+const student = {
+    name: 'Iliy',
+    age: 22
+}
+const veryHuge: number = BigInt(9007199254740991)
+let id = Symbol('id')
